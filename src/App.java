86bbd6634a8b3fc295582cc6c94b450d7f71b9c3
HongXin Huang <1061608626@qq.com>
2022-11-04 10:38:42 +0800

    动态居中

--- src/App.java
+++ src/App.java
@@ -22,17 +22,26 @@ public class App {
         pen.setColor(Color.BLACK);
 
         // 设置画笔样式 Font.PLAIN（正常） 0，Font.BOLD（粗体） 1，Font.ITALIC（斜体） 2
-        pen.setFont(new Font("黑体",0,30));
+        pen.setFont(new Font("黑体",1,40));
         // 添加文字
-        pen.drawString("巡检点位：2#车间名称",30,350);
+        pen.drawString("巡检点位：2#车间名称",30,275);
+        pen.drawString("单位/部门：",30,355);
+        pen.drawString("No：123",30,425);
+
+        pen.setFont(new Font("黑体",1,40));
+        // 动态居中效果
+        pen.drawString("静波集团有限公司",650 - (((40 + 2) * 8) / 2),70);
+
+        pen.setFont(new Font("黑体",1,50));
+        pen.drawString("巡检卡",575,145);
 
         // 读取二维码图片
         BufferedImage qrCode = ImageIO.read(new File("./static/baofeng/qrcode.png"));
         // 缩放二维码
-        Image qrCodeFormat = qrCode.getScaledInstance(250, 250, Image.SCALE_SMOOTH);
+        Image qrCodeFormat = qrCode.getScaledInstance(350, 350, Image.SCALE_SMOOTH);
 
         // 叠加二维码
-        pen.drawImage(qrCodeFormat, 500, 200, null);
+        pen.drawImage(qrCodeFormat, 600, 235, null);
 
         File file = new File("output.png");
         ImageIO.write(backImg, "png", file);
